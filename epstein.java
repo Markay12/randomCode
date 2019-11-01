6e1a9f55a01c8ca56f2c8f6fa1827d32b3943c73
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-11-01 07:33:22 -0700

    Funny epstein program

---
+++ epstein.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class epstein{
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    Random rnd = new Random();
+
+    int i = 0;
+
+    while(i != 100000){
+
+      System.out.print("Epstein didn't kill himself ");
+
+    }
+
+  }
+}
